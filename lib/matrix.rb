8aa2ed4cc79e8be7fd64d053920217ba589519ba
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-12 23:24:36 +0530

    add test to extract 0th row from '9 7\n8 6'

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,5 +1,5 @@
 class MatrixExtraction
   def row(matrix,rowValue)
-    [1,2]
+    matrix.split("\n")[0].split(' ').inject(Array.new) {|array,ch| array << ch.to_i}
   end
 end
--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -6,4 +6,9 @@ describe "MatrixExtraction" do
     mat = "1 2\n10 20"
     expect(matrix.row(mat,0)).to eq [1,2]
   end
+
+  it "test to extract 0th row from '9 7\\n8 6'" do
+    mat = "9 7\n8 6"
+    expect(matrix.row(mat,0)).to eq [9,7]
+  end
 end
